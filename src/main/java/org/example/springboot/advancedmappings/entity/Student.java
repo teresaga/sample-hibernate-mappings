4582e00f87ab74ebfde2738649602ac6c1e9104e
Teresa <teresagalaviza@gmail.com>
2025-03-10 16:38:45 -0700

    Map Student to Course

--- src/main/java/org/example/springboot/advancedmappings/entity/Student.java
+++ src/main/java/org/example/springboot/advancedmappings/entity/Student.java
@@ -2,6 +2,9 @@ package org.example.springboot.advancedmappings.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "student")
 public class Student {
@@ -19,6 +22,11 @@ public class Student {
 
     @Column(name = "email")
     private String email;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH},
+            mappedBy = "students")
+    private List<Course> courses;
 
     public Student() {
     }
@@ -59,6 +67,24 @@ public class Student {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void addCourse(Course course) {
+
+        if (this.courses == null) {
+            this.courses = new ArrayList<>();
+        }
+
+        this.courses.add(course);
+        course.addStudent(this);
     }
 
     @Override
