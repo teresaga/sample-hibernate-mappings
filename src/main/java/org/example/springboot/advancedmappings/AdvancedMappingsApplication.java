6ec88b95d46ec019c812ee7c7d656858ef499483
Teresa <teresagalaviza@gmail.com>
2025-03-10 16:24:17 -0700

    Student entity added

--- src/main/java/org/example/springboot/advancedmappings/AdvancedMappingsApplication.java
+++ src/main/java/org/example/springboot/advancedmappings/AdvancedMappingsApplication.java
@@ -23,23 +23,6 @@ public class AdvancedMappingsApplication {
     public CommandLineRunner commandLineRunner(AppDAO appDAO) {
 
         return runner -> {
-            //createInstructor(appDAO);
-            //findInstructor(appDAO);
-            //deleteInstructor(appDAO);
-            //findInstructorDetail(appDAO);
-            //deleteInstructorDetail(appDAO);
-            //createInstructorWithCourses(appDAO);
-            //findInstructorWithCourses(appDAO);
-            //findCoursesForInstructor(appDAO);
-            //findInstructorWithCoursesJoinFetch(appDAO);
-            //updateInstructor(appDAO);
-            //updateCourse(appDAO);
-            //deleteInstructor(appDAO);
-            //deleteCourse(appDAO);
-
-            //createCourseAndReviews(appDAO);
-            //retrieveCourseWithReviews(appDAO);
-            deleteCourseAndReviews(appDAO);
         };
     }
 
